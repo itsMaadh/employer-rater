522b88955adb2a5c4c3307ff5d3bca4ea9261c93
Maadh <github@maadh.pw>
2023-08-24 16:26:51 +0800

    fix: make sure prisma works

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ async function getEmployersData() {
 
 export default async function IndexPage() {
   const x = await getEmployersData()
-  console.log(x)
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
@@ -40,6 +39,7 @@ export default async function IndexPage() {
         >
           GitHub
         </Link>
+        {JSON.stringify(x)}
       </div>
     </section>
   )
