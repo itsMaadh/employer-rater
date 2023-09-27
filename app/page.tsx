21e171759fec30e97d2e0c24aa6b1fda9327d061
Maadh <github@maadh.pw>
2023-09-27 06:39:33 +0100

    feat: initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
 import prisma from "@/lib/prisma"
-import { buttonVariants } from "@/components/ui/button"
 
 async function getEmployersData() {
   return await prisma.employer.findMany()
@@ -11,36 +7,10 @@ async function getEmployersData() {
 export default async function IndexPage() {
   const x = await getEmployersData()
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex max-w-[980px] flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-        {JSON.stringify(x)}
-      </div>
-    </section>
+    <main className="container mx-auto py-8">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Dashboard
+      </h1>
+    </main>
   )
 }
--- config/site.ts
+++ config/site.ts
@@ -1,20 +1,15 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Employer Watcher",
+  name: "Employer Rater",
   description: "View and manage employers for Maldives Pension Office.",
   mainNav: [
-    {
-      title: "Dashboard",
-      href: "/",
-    },
     {
       title: "Reports",
       href: "/reports",
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
     github: "https://github.com/shadcn/ui",
     docs: "https://ui.shadcn.com",
   },
